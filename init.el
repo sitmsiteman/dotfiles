7fa1163e23a6b4043317fbdbd689d4183f81f2b6
sitmsiteman <107298363+sitmsiteman@users.noreply.github.com>
2024-10-05 19:50:31 +0900

    anaconda-mode

--- init.el
+++ init.el
@@ -232,6 +232,11 @@
   :config
   (company-quickhelp-mode))
 
+(use-package anaconda-mode
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook #'anaconda-mode))
+
 (use-package company-anaconda
   :ensure t
   :config
