d8a9dbdfa8fe1e90a79c62283a932a1ba7533c0c
sitmsiteman <107298363+sitmsiteman@users.noreply.github.com>
2025-03-11 03:28:31 +0900

    Update channels.scm

--- guix/channels.scm
+++ guix/channels.scm
@@ -8,4 +8,9 @@
               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
         (name 'guix)
-        (url "https://codeberg.org/guix/guix-mirror")))
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
